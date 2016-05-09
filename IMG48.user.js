2574b8d3e0c4d7933968b2b4c873d3fed1b070d0
hyww <hyww1101@gmail.com>
2016-05-09 23:04:02 +0800

    v0.3.0
    Check file type before downloading.
    Set @connect labels.

--- IMG48.user.js
+++ IMG48.user.js
@@ -3,11 +3,12 @@
 // @author      hyww13
 // @namespace   http://paruru.csie.org/IMG48.html
 // @downloadURL http://paruru.csie.org/IMG48.user.js
-// @version     0.2
+// @version     0.3
 // @description IMG48
 // @include     7gogo.jp/*
 // @include     twitter.com/*
 // @include     plus.google.com/*
+// @include     ngt48.com/photolog*
 // @include     ameblo.jp/*
 // @include     www.instagram.com/*
 // @match       7gogo.jp/*
@@ -16,7 +17,13 @@
 // @match       ngt48.com/photolog*
 // @match       ameblo.jp/*
 // @match       www.instagram.com/*
-// @connect     https://api.imgur.com
+// @connect     imgur.com
+// @connect     7gogo.jp
+// @connect     twimg.com
+// @connect     googleusercontent.com
+// @connect     ameba.jp
+// @connect     cdninstagram.com
+// @connect     ngt48.com
 // @require     http://code.jquery.com/jquery-2.1.4.min.js
 // @grant       GM_addStyle
 // @grant       GM_xmlhttpRequest
@@ -26,7 +33,6 @@
 
 (function() {
     'use strict';
-	
 	// http://stackoverflow.com/questions/12710001/how-to-convert-uint8-array-to-base64-encoded-string
 	function Uint8ToString(u8a){
 		var CHUNK_SZ = 0x8000;
@@ -36,10 +42,7 @@
 		}
 		return c.join("");
 	}
-	
 	var $imgContextmenu = $('<div id="imgContextmenu"><div class="options">Open</div><div class="options">Save</div><div class="options">Save As</div><div class="options IMG48img">Imgur</div></div>').appendTo('body').hide();
-	//var $imgLoading = $('<div id="imgLoading" class="spinkitSpinner"><div class="rect1"></div><div class="rect2"></div><div class="rect3"></div><div class="rect4"></div><div class="rect5"></div></div>').appendTo('body').hide();
-	// http://tobiasahlin.com/spinkit/
 	$(window).on('click', function(e){
 		console.log(e);
 		$('#imgContextmenu').hide();
@@ -50,33 +53,76 @@
 			url = url.replace(/(https?:\/\/stat.ameba.jp\/.+\/)(.+)_(.+)/, "$1o$3");	//ameblo
 			url = url.replace(/(https?:\/\/.+.cdninstagram.com\/.+?\/)((s\d+x\d+\/)?)([^?]+)(\?.*)?/, "$1$4");	//igs
 			var filename = null;
+			var type = null;
 			switch($(e.target).text()){
 				case 'Open':
 					window.open(url, '_blank');
 					break;
 				case 'Save As':
-					filename = prompt('Filename?', url.replace(/\.(jpg|png|gif)(:orig#.+|\?.+)$/i,'.$1').replace(/^.+\/(.+)/, "$1"));
+					filename = prompt('Filename?', url.replace(/(:orig#.+|\?.+)$/i,'').replace(/^.+\/(.+)/, "$1"));
 					if(!filename)break;
 				case 'Save':
-					if(!filename)filename = url.replace(/\.(jpe?g|png)(:orig#.+|\?.+)$/i,'.$1').replace(/^.+\/(.+)/, "$1");
-					if(!url.match(/.+\.(png|jpe?g).*/i)){
-						$('<a download="'+filename+'" href="'+url+'">')[0].click();
-						break;
+					if(!filename)filename = url.replace(/(:orig#.+|\?.+)$/i,'').replace(/^.+\/(.+)/, "$1");
+					var origname = url.replace(/(:orig#.+|\?.+)$/i,'').replace(/^.+\/(.+)/, "$1");
+					if(origname.match(/.+\.(png|bmp|jpeg|gif)$/i)){
+						type = 'image/'+origname.replace(/.+\.(png|bmp|jpeg|gif)$/i, "$1").toLowerCase();
 					}
-					GM_xmlhttpRequest({
-						method: "GET",
-						url: url,
-						overrideMimeType: "text/plain; charset=x-user-defined",
-						onload: function(response) {
-							var data = new Uint8Array(response.responseText.length);
-							var j = 0;
-							while(j < data.length){
-								data[j] = response.responseText.charCodeAt(j);
-								j++;
+					else if(origname.match(/.+\.jpg$/i)){
+						type = 'image/jpeg';
+					}
+					else if(origname.match(/.+\.mp4$/i)){
+						type = 'video/mp4';
+					}
+					console.log(type);
+					if(!type){
+						GM_xmlhttpRequest({
+							method: "HEAD",
+							url: url,
+							onload: function(response) {
+								console.log(response);
+								if(response.responseHeaders.match(/[\s\S]*content-type:\s+(.+)[\s\S]*/)){
+									type = response.responseHeaders.replace(/[\s\S]*content-type:\s+(.+)[\s\S]*/,"$1");
+									console.log('Get type '+type+' from header.');
+									GM_xmlhttpRequest({
+										method: "GET",
+										url: url,
+										overrideMimeType: "text/plain; charset=x-user-defined",
+										onload: function(response) {
+											var data = new Uint8Array(response.responseText.length);
+											var j = 0;
+											while(j < data.length){
+												data[j] = response.responseText.charCodeAt(j);
+												j++;
+											}
+											console.log('Download using data URI');
+											$('<a download="'+filename+'" href="data:'+type+';base64,'+btoa(Uint8ToString(data))+'">')[0].click();
+										}
+									});
+								}
+								else{
+									console.log('Direct download');
+									$('<a download="'+filename+'" href="'+url+'">')[0].click();
+								}
 							}
-							$('<a download="'+filename+'" href="data:image/'+(url.match(/.+\.png.*/i)?'png':'jpg')+';base64,'+btoa(Uint8ToString(data))+'">')[0].click();
-						}
-					});
+						});
+					}
+					else{
+						GM_xmlhttpRequest({
+							method: "GET",
+							url: url,
+							overrideMimeType: "text/plain; charset=x-user-defined",
+							onload: function(response) {
+								var data = new Uint8Array(response.responseText.length);
+								var j = 0;
+								while(j < data.length){
+									data[j] = response.responseText.charCodeAt(j);
+									j++;
+								}
+								console.log('Download using data URI');
+								$('<a download="'+filename+'" href="data:'+type+';base64,'+btoa(Uint8ToString(data))+'">')[0].click();
+							}
+						});
+					}
 					break;
 				case 'Imgur':
 					var win = window.open('', '_blank');
@@ -98,13 +144,12 @@
 							console.log(res);
 							var data = JSON.parse(res.responseText).data;
 							console.log(data);
-							//$imgLoading.hide();
 							if(data.error){
 								win.close();
 								$img.remove();
 								alert(data.error);
 							}
-							else{ //$img.on('load', function(){win.document.location.href=data.link;$(this).remove();}).attr('src',data.link);//window.open(data.link , '_blank');$(this).remove();});
+							else{
 								GM_xmlhttpRequest({
 									method: 'GET',
 									url: data.link,
@@ -148,6 +193,10 @@
 			url = $(e.target).parents('.poster-image-container').prevAll('.player-wrapper').find('video').attr('src');
 			$('.IMG48img').hide();
 		}
+		else if(e.target.matches('.poster-image-container')){	//twitter video thumb
+			url = $(e.target).prevAll('.player-wrapper').find('video').attr('src');
+			$('.IMG48img').hide();
+		}
 		else if(e.target.matches('.player-controls') || e.target.matches('.gif-play-pause')){	//twitter video and gif
 			url = $(e.target).prevAll('.player-wrapper').find('video').attr('src');
 			$('.IMG48img').hide();
@@ -168,9 +217,4 @@
 	'#imgContextmenu{z-index: 9999; position: fixed; background-color: white; width: 120px;}',
 	'.options{text-align: left; color: black; font-family: sans-serif; font-size: 15px; cursor: default; padding: 3px;}',
 	'.options:hover{background-color: #2196F3;}'].join(''));
-	//'#imgLoading{z-index: 9999; position: fixed; left: 50%; top: 50%; background: none!important;}'].join(''));
-	
-	// http://tobiasahlin.com/spinkit/
-	//GM_addStyle('.spinkitSpinner { width: 75px; height: 60px; text-align: center; font-size: 10px; } .spinkitSpinner > div { background-color: #fb4c42; height: 100%; width: 9px; display: inline-block; -webkit-animation: sk-stretchdelay 1.2s infinite ease-in-out; animation: sk-stretchdelay 1.2s infinite ease-in-out; } .spinkitSpinner .rect2 { -webkit-animation-delay: -1.1s; animation-delay: -1.1s; } .spinkitSpinner .rect3 { -webkit-animation-delay: -1.0s; animation-delay: -1.0s; } .spinkitSpinner .rect4 { -webkit-animation-delay: -0.9s; animation-delay: -0.9s; } .spinkitSpinner .rect5 { -webkit-animation-delay: -0.8s; animation-delay: -0.8s; } @-webkit-keyframes sk-stretchdelay { 0%, 40%, 100% { -webkit-transform: scaleY(0.4) } 20% { -webkit-transform: scaleY(1.0) } } @keyframes sk-stretchdelay { 0%, 40%, 100% { transform: scaleY(0.4); -webkit-transform: scaleY(0.4); } 20% { transform: scaleY(1.0); -webkit-transform: scaleY(1.0); } }');
-	
 })();
